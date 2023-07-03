b529aa34177fb75a9471425edea2a07e879f0adb
patrickgustafson <patrickrpgustafson@gmail.com>
2023-07-03 18:17:53 -0500

    dto and mapper implementation

--- src/main/java/com/skillstorm/project1/controllers/WarehouseController.java
+++ src/main/java/com/skillstorm/project1/controllers/WarehouseController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.skillstorm.project1.dtos.WarehouseDto;
 import com.skillstorm.project1.models.Warehouse;
 import com.skillstorm.project1.services.WarehouseService;
 
@@ -31,7 +32,7 @@ public class WarehouseController {
     }
 
     @GetMapping
-    public List<Warehouse> findAllWarehouses() {
+    public List<WarehouseDto> findAllWarehouses() {
         return service.findAllWarehouses();
     }
 
