5a3acb85155dc4c6d3d6a95d96261439325bd737
patrickgustafson <patrickrpgustafson@gmail.com>
2023-07-12 21:16:55 -0400

    Stock classes changes

--- src/main/java/com/skillstorm/project1/controllers/StockController.java
+++ src/main/java/com/skillstorm/project1/controllers/StockController.java
@@ -1,12 +1,90 @@
 package com.skillstorm.project1.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.skillstorm.project1.models.Stock;
+import com.skillstorm.project1.repositories.StockRepository;
 import com.skillstorm.project1.services.StockService;
+import com.skillstorm.project1.services.WarehouseService;
+import com.skillstorm.project1.services.ItemService;
+
 
 public class StockController {
     
     @Autowired
-    StockService service;
+    StockService stockService;
 
+    @Autowired
+    StockRepository stockRepository;
+
+    @Autowired
+    ItemService itemService;
+
+    @Autowired
+    WarehouseService warehouseService;
+
+    @GetMapping
+    public ResponseEntity<List<Stock>> getAllStock() {
+        
+        List<Stock> stock = stockService.findAllStock();
+        return new ResponseEntity<>(stock, HttpStatus.OK);
+    }
+
+    @GetMapping("/{itemId}/{warehouseId}")
+    public ResponseEntity<Stock> findStockByIds(@PathVariable int itemId, @PathVariable int warehouseId) {
+
+        Stock stock = stockService.findStockByIds(itemId, warehouseId);
+        return new ResponseEntity<Stock>(stock, HttpStatus.OK);
+    }
+
+    @GetMapping("/warehouse/{warehouseId}")
+    public ResponseEntity<List<Stock>> getStockByWarehouse(@PathVariable int warehouseId) {
+
+        List<Stock> stocks = stockService.findStockByWarehouse(warehouseId);
+        return new ResponseEntity<>(stocks, HttpStatus.OK);
+    }
+
+    @GetMapping("/item/{itemId}")
+    public ResponseEntity<List<Stock>> getStockByItem(@PathVariable int itemId) {
+
+        List<Stock> stocks = stockService.findStockByItem(itemId);
+        return new ResponseEntity<>(stocks, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Stock> createStock(
+            @RequestParam("itemId") int itemId,
+            @RequestParam("warehouseId") int warehouseId,
+            @RequestParam("quantity") int quantity) {
+
+        Stock stock = stockService.createStock(itemId, warehouseId, quantity);
+        return new ResponseEntity<Stock>(stock, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{itemId}/{warehouseId}")
+    public ResponseEntity<Integer> deleteStock(@PathVariable int itemId,
+            @PathVariable int warehouseId) {
+
+        int deletedStock = stockService.deleteStock(itemId, warehouseId);
+        return new ResponseEntity<Integer>(deletedStock, HttpStatus.OK);
+    }
+
+    @PutMapping("/{stockId}/{warehouseId}/{quantity}")
+    public ResponseEntity<Integer> updateStock(@PathVariable int itemId,
+            @PathVariable int warehouseId,
+            @PathVariable(required = false) int quantity) {
+
+        int updatedStock = stockService.updateStock(itemId, warehouseId, quantity);
+        return new ResponseEntity<Integer>(updatedStock, HttpStatus.OK);
+    }
 }
--- src/main/java/com/skillstorm/project1/repositories/StockRepository.java
+++ src/main/java/com/skillstorm/project1/repositories/StockRepository.java
@@ -1,11 +1,21 @@
 package com.skillstorm.project1.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.skillstorm.project1.models.Item;
 import com.skillstorm.project1.models.Stock;
+import com.skillstorm.project1.models.StockId;
+import com.skillstorm.project1.models.Warehouse;
 
 @Repository
 public interface StockRepository extends JpaRepository<Stock, Integer> {
-    
+    Optional<Stock> findById(StockId id);
+
+    Optional<List<Stock>> findByWarehouse(Warehouse warehouseId);
+
+    Optional<List<Stock>> findByItem(Item item);
 }
